d4992eddf5e7e06e5e5723b0ec6c5d0b9e47e383
horben1177 <ecu1337@gmail.com>
2017-02-27 08:14:43 -0800

    bugfix4

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -10,7 +10,7 @@ public class TrainControllerImpl implements TrainController {
 
 	@Override
 	public void followSpeed() {
-		if (referenceSpeed < 0) {
+		if (referenceSpeed <= 0) {
 			referenceSpeed = 0;
 		} else {
 			referenceSpeed += step;
